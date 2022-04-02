3ff8e45b62d0091e6f8b9236100f1163744f1601
mamunabdullah90 <mamun.mbsc90@gmail.com>
2022-04-03 04:51:47 +0600

    Ramadan Mubarak Web

---
+++ script.js
@@ -0,0 +1,41 @@
+const sounds = [
+  'song1',
+  'song2',
+  'song3',
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+  document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
